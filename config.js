0d37a28cdf284c402e5ca219317817d8a0979c1e
ImnotMiguel <miguelantonioanguiano@gmail.com>
2024-11-18 11:38:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
   },
-  users: {
+  users: {git clone --branch Ad-Free https://github.com/UseInterstellar/Interstellar
+cd Interstellar
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",
   },
